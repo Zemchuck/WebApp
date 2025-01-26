e7afa248590fb423e65d6c00a908cbde25585982
Zemchuck <zemczakk@gmail.com>
2025-01-26 15:07:41 +0100

    final project

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
@@ -7,10 +7,43 @@ import MoviesList from "./MoviesList";
 function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
+// Add movie function
+    async function handleAddMovie(movie) {
+        const response = await fetch('/movies', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'}
+            });
+        if (response.ok) {
+            const movieFromServer = await response.json();
+            setMovies([...movies, movieFromServer]);
+            setAddingMovie(false);
+        }
+    }
+//Show movies function
+    useEffect (() => {
+        async function fetchMovies() {
+            const response = await fetch('/movies');
+            if (response.ok) {
+                const movies = await response.json();
+                setMovies(movies);
+            }
+        }
+        fetchMovies();
+    }, 
+    []);
+// Delete movie function
+    async function handleDeleteMovie(movie){
+        const response = await fetch(`/movies/${movie.id}`, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            const nextMovies = movies.filter(m => m !== movie);
+            setMovies(nextMovies);
+        }
+    
 
-    function handleAddMovie(movie) {
-        setMovies([...movies, movie]);
-        setAddingMovie(false);
     }
 
     return (
@@ -19,13 +52,15 @@ function App() {
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                              onDeleteMovie={handleDeleteMovie}
                 />}
+
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
                              buttonLabel="Add a movie"
                 />
-                : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+                : <button onClick={() => setAddingMovie(true)}>Add a movie</button>
+                }
         </div>
     );
 }
